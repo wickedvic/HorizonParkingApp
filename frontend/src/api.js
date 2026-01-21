9278187692bd5cdfcb2d71fa80b153dac20e68a2
Vivek Sanghvi <vivek1019@gmail.com>
2026-01-21 16:53:46 -0500

    updated server.js file and nginx.conf file to fix route error connections

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,7 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-export default API_BASE_URL
+// export default API_BASE_URL
+
+const API_BASE_URL = "/api"; 
+export default API_BASE_URL;
  
